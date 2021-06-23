1672db39263c463d920cded69c6a313682d6485b
Vishal <vishal@localhost.localdomain>
2021-06-23 11:05:02 +0530

    Fixed Search

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
+Project Status : On Going...
 Site is live at : 
 Api used from : http://www.omdbapi.com/apikey.aspx 
+
+
+In This Peoject I Have Used and Implemented:
+
+1. bootstrap
+2. useState hook
+3. useEffect hook
+4. Fetch with async/await
+5. Live Search 
--- src/App.js
+++ src/App.js
@@ -2,30 +2,41 @@ import "./App.css";
 import React, { useEffect, useState } from "react";
 import MovieList from "./components/MovieList";
 import "bootstrap/dist/css/bootstrap.min.css";
+import MovieListHeading from "./components/MovieListHeading";
+import SearchBox from "./components/SearchBox";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState([]);
 
-  const getMovieRequest = async() => {
-    const url = 'http://www.omdbapi.com/?s=star wars&apikey=8367cb5a';
+
+  const getMovieRequest = async(searchValue) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=8367cb5a`; //Template literals (Template strings)
+
 
     const response = await fetch(url);
     const responseJson = await response.json();
 
-    console.log(responseJson);
-    setMovies(responseJson.Search);
+    if(responseJson.Search)
+    {
+      setMovies(responseJson.Search);
+    }
+    // console.log(responseJson);
   }
 
 useEffect(()=>{
-  getMovieRequest();
-
-},[]);
+  getMovieRequest(searchValue);
+},[searchValue]);
 
 
 
 
   return (
     <div className="container-fluid movie-app">
+    <div className="row d-flex align-items-center mb-4 mt-4">
+      <MovieListHeading heading = 'Movies' />
+      <SearchBox searchValue = {searchValue}  setSearchValue={setSearchValue}/>
+    </div>
       <div className="row">
         <MovieList movies={movies} />
       </div>
